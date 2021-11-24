8f1615df5f2da92d9c4ad0ec4c518e14ac04782e
WeiBin <wy9077@rit.edu>
2021-11-24 04:00:42 -0800

    upload final code without node_modules

---
+++ final_code/react_startcode/src/components/Table/index.jsx
@@ -0,0 +1,13 @@
+import {useEffect,useState} from 'react';
+
+function Table(){
+
+    return (
+        <div>
+            <p>Hello World</p>
+            <p>This is Table Component</p>
+        </div>
+    );//return
+}//Table
+
+export default Table; 
