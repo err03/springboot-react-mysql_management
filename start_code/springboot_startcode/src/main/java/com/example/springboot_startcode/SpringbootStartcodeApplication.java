84027bc88ea5eecf371103f8ed7bd4148e714f09
WeiBin <wy9077@rit.edu>
2021-11-24 01:43:05 -0800

    add start code and final code

---
+++ start_code/springboot_startcode/src/main/java/com/example/springboot_startcode/SpringbootStartcodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_startcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStartcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStartcodeApplication.class, args);
+	}
+
+}
---
+++ start_code/springboot_startcode/src/test/java/com/example/springboot_startcode/SpringbootStartcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_startcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStartcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
