1a6b003fb76e1544df66f5f626d4202e2cbece48
WeiBin <wy9077@rit.edu>
2021-11-28 14:48:57 -0800

    final code update

---
+++ final_code/springboot_demo/src/main/java/com/example/springboot_demo/SpringbootDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.springboot_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoApplication.class, args);
+	}
+
+}
---
+++ final_code/springboot_demo/src/main/java/com/example/springboot_demo/student/Student.java
@@ -0,0 +1,13 @@
+package com.example.springboot_demo.student;
+
+import lombok.Data;
+
+@Data
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+    private double grade;
+
+    //getter and setter
+}
